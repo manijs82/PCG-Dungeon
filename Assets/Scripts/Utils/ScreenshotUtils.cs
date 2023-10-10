e2ce3b6eef7a92fb1e68bb465a01e51f2b5f3c30
Mani <Manijavdani1@gmail.com>
2023-10-10 14:01:23 +0200

    memory profiler package added

--- Assets/Scripts/Utils/ScreenshotUtils.cs
+++ Assets/Scripts/Utils/ScreenshotUtils.cs
@@ -13,13 +13,25 @@ namespace Utils
             int dungeonWidth = dungeon.dungeonParameters.width;
             int dungeonHeight = dungeon.dungeonParameters.width;
 
-            Texture2D texture = new Texture2D(dungeonWidth * pixelPerUnit, dungeonHeight * pixelPerUnit);
+            var texture = GetTilemapTexture(tilemap, pixelPerUnit, dungeonWidth, dungeonHeight);
+
+            byte[] bytes = texture.EncodeToPNG();
+            if (Directory.Exists(path))
+            {
+                string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
+                File.WriteAllBytes(path + $"Screenshot_{dateTime}.png", bytes);
+            }
+        }
+
+        public static Texture2D GetTilemapTexture(Tilemap tilemap, int pixelPerUnit, int width, int height)
+        {
+            Texture2D texture = new Texture2D(width * pixelPerUnit, height * pixelPerUnit);
             texture.filterMode = FilterMode.Point;
             texture.wrapMode = TextureWrapMode.Clamp;
 
-            for (int x = 0; x < dungeonWidth; x++)
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < dungeonHeight; y++)
+                for (int y = 0; y < height; y++)
                 {
                     Sprite sprite = tilemap.GetSprite(new Vector3Int(x, y));
                     Texture2D spriteTexture = sprite.texture;
@@ -33,20 +45,16 @@ namespace Utils
                         {
                             Vector2Int scaledPixelCoordinate = GetNearestPixelCoordinate(new Vector2Int(i, j),
                                 sprite.pixelsPerUnit / pixelPerUnit);
-                            Color pixelColor = spriteTexture.GetPixel(atlasX + scaledPixelCoordinate.x, atlasY + scaledPixelCoordinate.y) *
-                                               tilemap.GetColor(new Vector3Int(x, y));
+                            Color pixelColor =
+                                spriteTexture.GetPixel(atlasX + scaledPixelCoordinate.x, atlasY + scaledPixelCoordinate.y) *
+                                tilemap.GetColor(new Vector3Int(x, y));
                             texture.SetPixel(x * pixelPerUnit + i, y * pixelPerUnit + j, pixelColor);
                         }
                     }
                 }
             }
 
-            byte[] bytes = texture.EncodeToPNG();
-            if (Directory.Exists(path))
-            {
-                string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss");
-                File.WriteAllBytes(path + $"Screenshot_{dateTime}.png", bytes);
-            }
+            return texture;
         }
 
         private static Vector2Int GetNearestPixelCoordinate(Vector2Int coordinate, float scale)
